81633c756d99df1525d87c1715c6f745ef173b25
Misia Abdipatra <misiabdipatra@gmail.com>
2024-10-15 20:22:06 +0700

    update_

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 <nav class="navbar">
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Astronot</a>
+    <a class="navbar-brand" href="#">AstroEdu</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -41,7 +41,6 @@
 </nav>
 
 <!-- Navbar End -->
-
 
 
 </body>
