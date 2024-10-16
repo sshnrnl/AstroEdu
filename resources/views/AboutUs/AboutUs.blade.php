e52045c5960dcf151472a5efb9ab0d22cb28e6de
unknown <billykurniawan9@gmail.com>
2024-10-16 17:26:08 +0700

    Card

--- resources/views/AboutUs/AboutUs.blade.php
+++ resources/views/AboutUs/AboutUs.blade.php
@@ -60,6 +60,7 @@
     </nav>
 <!-- Navbar End -->
 
+  <!-- Main Start -->
     <main>
         <div class="container">
             <div class="row">
@@ -85,5 +86,44 @@
             </div>
         </div>
     </main>
+    <!-- Main End -->
+
+    <!-- Card Start -->
+     <div class="card__container">
+        <div class="card_container">
+          <article class="card_article">
+            <img src="{{ asset('Login/Card1.png') }}" alt="image" class="card_img">
+
+            <div class="card_data">
+              <span class="card_desc">Games</span>
+                <h2 class="card_title">Astronot Satu</h2>
+                <a href="#" class="card_button">Read More</a>
+            </div>
+
+          </article>
+          <article class="card_article">
+            <img src="{{ asset('Login/Card2.png') }}" alt="image" class="card_img">
+
+            <div class="card_data">
+              <span class="card_desc">Project</span>
+                <h2 class="card_title">Astronot Dua</h2>
+                <a href="#" class="card_button">Read More</a>
+            </div>
+
+          </article>
+          <article class="card_article">
+            <img src="{{ asset('Login/Card3.png') }}" alt="image" class="card_img">
+
+            <div class="card_data">
+              <span class="card_desc">Students</span>
+                <h2 class="card_title">Astronot Tiga</h2>
+                <a href="#" class="card_button">Read More</a>
+            </div>
+
+          </article>
+
+        </div>
+     </div>
+    <!-- Card End -->
 </body>
 </html>
