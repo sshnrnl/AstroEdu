5e4e81b50e46d7bde6da60e04867fc3ce1096b53
Misia Abdipatra <misiabdipatra@gmail.com>
2024-11-26 18:07:31 +0700

    update

--- resources/views/Header_Footer/footer.blade.php
+++ resources/views/Header_Footer/footer.blade.php
@@ -10,42 +10,30 @@
 <body>
     
     <!-- Footer -->
-    <footer>
-    <div class="container-fluid d-flex justify-content-around p-5" style="background-color: #2F387B">
-      <div class="d-flex flex-column align-items-center">
-        <img class="footer_logo" src="{{ asset('Header_Footer/Astronot.png') }}" alt="">
-      </div>
-  
-      <div class="">
-        <h3 class="text-light fw-bold">Site map</h3>
-  
-        <div class="d-flex m-0 p-0">
-          <div class="container d-flex flex-column m-0 p-0">
-            <a href="" class="footer_text text-decoration-none text-light">Store</a>
-            <a href="" class="footer_text text-decoration-none text-light">Profile</a>
-            <a href="" class="footer_text text-decoration-none text-light">Home</a>
-          </div>
+    <footer style="background-color: #02090E; color: #fff; padding: 20px 0;">
+        <div class="container text-center">
+            <!-- Social Media Icons -->
+            <div class="mb-4">
+                <a href="#" class="mx-2"><img src="{{ asset('Header_Footer/Facebook Logo.png') }}" width="40" alt="Facebook"></a>
+                <a href="#" class="mx-2"><img src="{{ asset('Header_Footer/Instagram Logo.png') }}" width="40" alt="Instagram"></a>
+                <a href="#" class="mx-2"><img src="{{ asset('Header_Footer/x.png') }}" width="40" alt="Twitter"></a>
+                <a href="#" class="mx-2"><img src="{{ asset('Header_Footer/youtube.png') }}" width="40" alt="YouTube"></a>
+            </div>
+            
+            <!-- Navigation Links -->
+            <div class="mb-2">
+                <a href="#" class="text-decoration-none text-light mx-3">Home</a>
+                <a href="{{ url('/learn') }}" class="text-decoration-none text-light mx-3">Learning</a>
+                <a href="{{ url('/community') }}" class="text-decoration-none text-light mx-3">Community</a>
+            </div>
         </div>
-  
-      </div>
-  
-      <div class="">
-        <h3 class="text-light fw-bold" style="padding-left: 30px;">Follow kami di:</h3>
-        <ul class="d-flex gap-4" style="list-style-type: none;">
-          <img src="{{ asset('Header_Footer/youtube.png') }}" width="50" height="40" alt="">
-          <img src="{{ asset('Header_Footer/x.png') }}" width="70" height="40" alt="">
-          <img src="{{ asset('Header_Footer/Facebook Logo.png') }}" width="50" height="40" alt="">
-          <img src="{{ asset('Header_Footer/Instagram Logo.png') }}" width="50" height="40" alt="">
-        </ul>
-      </div>
-    </div>
-  
-  
-    <div class="d-flex justify-content-center p-2" style="background: #152244">
-      <span class="text-light">All rights reserved. &copy; 2024 AstroEdu.ID</span>
-    </div>
-  
   </footer>
+            <div class="d-flex justify-content-center p-2" style="background: #152244;  color: #fff">
+              <span>All rights reserved. &copy; 2024 AstroEdu.ID</span>
+            </div>
+        
+  
+  
 
 </body>
 </html>
--- resources/views/Header_Footer/header.blade.php
+++ resources/views/Header_Footer/header.blade.php
@@ -10,6 +10,8 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
+
+      <div class="d-flex align-items-center">
         <a class="navbar-brand" href="#">
           <img
             src="{{ asset('Header_Footer/Astronot.png') }}"
@@ -19,6 +21,8 @@
           />
         </a>
         <span class="fs-3 fw-semibold text-light">AstroEdu</span>
+        </div>
+        
         <button
           class="navbar-toggler"
           type="button"
@@ -31,10 +35,10 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div
-          class="collapse navbar-collapse justify-content-center"
+          class="collapse navbar-collapse justify-content-between"
           id="navbarNav"
         >
-          <ul class="navbar-nav">
+          <ul class="navbar-nav mx-auto">
             <li class="nav-item">
               <a class="nav-link text-decoration-none text-light" aria-current="page" href="#">Home</a>
             </li>
@@ -44,13 +48,18 @@
             <li class="nav-item">
               <a class="nav-link text-decoration-none text-light" href="{{ url('/community') }}">Community</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-decoration-none text-light" href="{{ url('/') }}">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-decoration-none text-light" href="{{ url('/profil') }}">Profile</a>
-            </li>
           </ul>
+
+            <div class="d-flex align-items-center">
+                <button type="button" class="btn btn-warning me-3" href="{{ url('/') }}">Sign In</button>
+                <a href="{{ url('/profil') }}" class="text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                    </svg>
+                </a>
+            </div>
+          
         </div>
       </div>
     </nav>
